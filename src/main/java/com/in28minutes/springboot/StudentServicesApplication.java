28018bb87f55f3b275816de666ed3d3502e5a63f
Purusothaman Ramanujam <purushoth.r@gmail.com>
2021-02-11 23:49:27 +0530

    Wildfly

--- src/main/java/com/in28minutes/springboot/StudentServicesApplication.java
+++ src/main/java/com/in28minutes/springboot/StudentServicesApplication.java
@@ -2,11 +2,19 @@ package com.in28minutes.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StudentServicesApplication {
+public class StudentServicesApplication extends SpringBootServletInitializer {
+	private static Class<StudentServicesApplication> applicationClass = StudentServicesApplication.class;
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudentServicesApplication.class, args);
+		SpringApplication.run(applicationClass, args);
 	}
 }
